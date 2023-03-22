b8662aff3036f5006183878263855858844b1bf3
anil-650 <anilhansda65@gmail.com>
2023-03-22 12:01:39 +0530

    Feature: add dashboard route - add /dashboard to index.js - add dashboard.js to routes dir - add authrization middleware to dashboard.js - fix spelling in index.js

--- server/index.js
+++ server/index.js
@@ -6,10 +6,13 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-// Regisration
+// Authorization route
 app.use("/auth", require("./routes/jwtAuth"));
 
-// ROUTES
+// Dashboard route
+app.use("/dashboard", require("./routes/dashboard"));
+
+// check server running
 app.get("/", (req, res)=>{
     res.json({message: "hello from server"});
 });
