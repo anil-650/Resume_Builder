0a4d53b73cf570c8ebc68921dcf2d3216831db1c
anil-650 <anilhansda65@gmail.com>
2023-03-27 14:59:00 +0530

    Update: add front-end beta
    - add beta-client made with astro

---
+++ beta-client/astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+  experimental: {
+    assets: true
+  },
+  integrations: [tailwind()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+});
