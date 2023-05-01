b910ab72c69462883e638da2e25276ba9acb0aa1
anil-650 <anilhansda65@gmail.com>
2023-05-01 05:54:04 +0000

    Update download pdf working from server side
    - add resume route to index.js
    - add puppeteer and gcloud storage to server
    - add resume route for saving resumes and pdf generation

--- server/index.js
+++ server/index.js
@@ -18,10 +18,13 @@ app.use("/templates", require("./routes/templates"));
 
 app.use("/resume", require("./routes/resume"));
 
+app.use("/pup", require("./routes/pup"));
+
 // check server running
 app.get("/", (req, res)=>{
     res.json({message: "hello from server"});
 });
+
 
 // SERVER START
 app.listen(5000, ()=> {
