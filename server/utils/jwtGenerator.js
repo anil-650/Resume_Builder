cf6f66fafaab3153c4ac689618b2448c5125ff59
anil-650 <anilhansda65@gmail.com>
2023-05-13 15:12:23 +0000

    Update: modify utils/jwtGenerator
    - jwtGenerator() now takes token_time parameter to specify the expire duration

--- server/utils/jwtGenerator.js
+++ server/utils/jwtGenerator.js
@@ -1,15 +1,15 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-function jwtGenerator(user_id) {
+function jwtGenerator(user_id, token_time) {
    const payload = {
     user: {
         id: user_id
     }
    };
 
-    // CURRENTLY 2hrs timeout
-   return jwt.sign(payload, process.env.jwtSecret, {expiresIn: "2hr"});
+    // TOKEN_TIME TO TIME OUT THE TOKEN
+   return jwt.sign(payload, process.env.jwtSecret, {expiresIn: token_time});
 }
 
 module.exports = jwtGenerator;
