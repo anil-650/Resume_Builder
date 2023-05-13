96b4462eb6664ce492f7826d9a140ac0f24e5d6a
anil-650 <anilhansda65@gmail.com>
2023-05-13 15:40:55 +0000

    Update: add new package astro-compress
    - add astro-compress for image css and html compression

--- beta-client/astro.config.mjs
+++ beta-client/astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import node from "@astrojs/node";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     assets: true
   },
-  integrations: [tailwind()],
+  integrations: [tailwind(), compress()],
   output: "server",
   adapter: node({
     mode: "standalone"
