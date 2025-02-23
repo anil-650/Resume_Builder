02e694aec4eba211b20bb3109758c7dddb3ce34f
anil-650 <anilhansda65@gmail.com>
2025-02-23 17:48:03 +0530

    PATCH: remove astro compress in config, no logner needed

--- beta-client/astro.config.mjs
+++ beta-client/astro.config.mjs
@@ -2,18 +2,15 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
 
-import compress from "astro-compress";
-
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    assets: true
-  },
-  integrations: [tailwind(), compress({
-      css: { restructure: false }
-      })],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+	experimental: {
+		assets: true
+	},
+	integrations: [tailwind()],
+	output: "server",
+	server: { port: 4321 },
+	adapter: node({
+		mode: "standalone"
+	})
 });
